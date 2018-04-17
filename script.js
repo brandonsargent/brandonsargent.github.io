1b47234d9e2bc4b1ca77ea41281504e06bc52d1c
Gabrielle Guarino <gguarino@umich.edu>
2018-04-16 22:00:36 -0400

    added about route

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+
+$('#brandon-txt').load("text/brandon.txt");
+$('#jake-txt').load("text/jake.txt");
+$('#sam-txt').load("text/sam.txt");
+$('#chelsea-txt').load("text/chelsea.txt");
+
+
 var app = angular.module('website', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/home', {
       templateUrl: 'home.html'
+    })
+    .when('/about', {
+      templateUrl: 'about.html'
     })
     .when('/cast', {
       templateUrl: 'cast.html'
@@ -18,7 +28,3 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('main-controller', ['$scope', function($scope){
 }]);
 
-$('#brandon-txt').load("text/brandon.txt");
-$('#jake-txt').load("text/jake.txt");
-$('#sam-txt').load("text/sam.txt");
-$('#chelsea-txt').load("text/chelsea.txt");
